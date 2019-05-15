91f82a9221b2e87930be7521e94b08897d74c9d7
ddang9390 <dnld1ng@yahoo.com>
2019-05-15 14:48:31 -0400

    Seperated forms into its own component

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
